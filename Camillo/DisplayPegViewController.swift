21de8c6e7aaf9ef1c17ffcee5a7857339f2cebb5
Frank Bassard <f.bassard@gmail.com>
2015-06-24 16:47:20 +1200

    Pegs design

--- Camillo/DisplayPegViewController.swift
+++ Camillo/DisplayPegViewController.swift
@@ -12,11 +12,27 @@ class DisplayPegViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.title = "Peg"
+        
+        if !UIAccessibilityIsReduceTransparencyEnabled() {
+            let blurEffect = UIBlurEffect(style: UIBlurEffectStyle.Dark)
+            let blurEffectView = UIVisualEffectView(effect: blurEffect)
+            blurEffectView.frame = view.bounds
+            view.addSubview(blurEffectView)
+            
+            blurEffectView.setTranslatesAutoresizingMaskIntoConstraints(false)
+            view.addConstraint(NSLayoutConstraint(item: blurEffectView, attribute: NSLayoutAttribute.Top, relatedBy: NSLayoutRelation.Equal, toItem: view, attribute: NSLayoutAttribute.Top, multiplier: 1, constant: 0))
+            view.addConstraint(NSLayoutConstraint(item: blurEffectView, attribute: NSLayoutAttribute.Height, relatedBy: NSLayoutRelation.Equal, toItem: nil, attribute: NSLayoutAttribute.NotAnAttribute, multiplier: 1, constant: 50))
+            view.addConstraint(NSLayoutConstraint(item: blurEffectView, attribute: NSLayoutAttribute.Leading, relatedBy: NSLayoutRelation.Equal, toItem: view, attribute: NSLayoutAttribute.Leading, multiplier: 1, constant: 0))
+            view.addConstraint(NSLayoutConstraint(item: blurEffectView, attribute: NSLayoutAttribute.Trailing, relatedBy: NSLayoutRelation.Equal, toItem: view, attribute: NSLayoutAttribute.Trailing, multiplier: 1, constant: 0))
+        }
+        
         key.text = peg.valueForKey("key") as? String
         name.text = peg.valueForKey("name") as? String
         
         var data = peg.valueForKey("image") as? NSData
         image.image =  UIImage(data: data!)
     }
+    
     
 }
