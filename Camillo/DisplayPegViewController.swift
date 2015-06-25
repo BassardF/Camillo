b42a1a488eec1ab88889b10ed678c0a5558fc16a
Frank Bassard <f.bassard@gmail.com>
2015-06-25 17:04:28 +1200

    Blur effect

--- Camillo/DisplayPegViewController.swift
+++ Camillo/DisplayPegViewController.swift
@@ -14,28 +14,40 @@ class DisplayPegViewController: UIViewController {
         self.navigationController!.navigationBar.hidden = true
         
         if !UIAccessibilityIsReduceTransparencyEnabled() {
-            let blurEffect = UIBlurEffect(style: UIBlurEffectStyle.Dark)
-            let blurEffectView = UIVisualEffectView(effect: blurEffect)
-            blurEffectView.frame = view.bounds
+            /*
+            //view.addConstraint(NSLayoutConstraint(item: blurEffectView, attribute: NSLayoutAttribute.Top, relatedBy: NSLayoutRelation.Equal, toItem: view, attribute: NSLayoutAttribute.Top, multiplier: 1, constant: 0))
+            //view.addConstraint(NSLayoutConstraint(item: blurEffectView, attribute: NSLayoutAttribute.Height, relatedBy: NSLayoutRelation.Equal, toItem: nil, attribute: NSLayoutAttribute.NotAnAttribute, multiplier: 1, constant: 50))
+            //view.addConstraint(NSLayoutConstraint(item: blurEffectView, attribute: NSLayoutAttribute.Bottom, relatedBy: NSLayoutRelation.Equal, toItem: view, attribute: NSLayoutAttribute.Bottom, multiplier: 1, constant: 0))
+            //view.addConstraint(NSLayoutConstraint(item: blurEffectView, attribute: NSLayoutAttribute.Leading, relatedBy: NSLayoutRelation.Equal, toItem: view, attribute: NSLayoutAttribute.Leading, multiplier: 1, constant: 0))
+            //view.addConstraint(NSLayoutConstraint(item: blurEffectView, attribute: NSLayoutAttribute.Trailing, relatedBy: NSLayoutRelation.Equal, toItem: view, attribute: NSLayoutAttribute.Trailing, multiplier: 1, constant: 0))
+            */
+            
+            // Blur Effect
+            var blurEffect = UIBlurEffect(style: UIBlurEffectStyle.Dark)
+            var blurEffectView = UIVisualEffectView(effect: blurEffect)
+            blurEffectView.frame = CGRectMake(0 , 0, self.view.frame.width, 600)
             view.addSubview(blurEffectView)
             
-            blurEffectView.setTranslatesAutoresizingMaskIntoConstraints(false)
-            view.addConstraint(NSLayoutConstraint(item: blurEffectView, attribute: NSLayoutAttribute.Top, relatedBy: NSLayoutRelation.Equal, toItem: view, attribute: NSLayoutAttribute.Top, multiplier: 1, constant: 0))
-            view.addConstraint(NSLayoutConstraint(item: blurEffectView, attribute: NSLayoutAttribute.Height, relatedBy: NSLayoutRelation.Equal, toItem: nil, attribute: NSLayoutAttribute.NotAnAttribute, multiplier: 1, constant: 50))
-            view.addConstraint(NSLayoutConstraint(item: blurEffectView, attribute: NSLayoutAttribute.Leading, relatedBy: NSLayoutRelation.Equal, toItem: view, attribute: NSLayoutAttribute.Leading, multiplier: 1, constant: 0))
-            view.addConstraint(NSLayoutConstraint(item: blurEffectView, attribute: NSLayoutAttribute.Trailing, relatedBy: NSLayoutRelation.Equal, toItem: view, attribute: NSLayoutAttribute.Trailing, multiplier: 1, constant: 0))
+            // Vibrancy Effect
+            var vibrancyEffect = UIVibrancyEffect(forBlurEffect: blurEffect)
+            var vibrancyEffectView = UIVisualEffectView(effect: vibrancyEffect)
+            vibrancyEffectView.frame = view.bounds
             
-            let vibrancyView: UIVisualEffectView = UIVisualEffectView(effect: UIVibrancyEffect(forBlurEffect: blurEffect))
-            vibrancyView.setTranslatesAutoresizingMaskIntoConstraints(false)
-            blurEffectView.contentView.addSubview(vibrancyView)
+            // Label for vibrant text
+            var vibrantLabel = UILabel()
+            vibrantLabel.text = "Vibrant"
+            vibrantLabel.font = UIFont.systemFontOfSize(12.0)
+            vibrantLabel.sizeToFit()
+            vibrantLabel.center = view.center
             
-            var label: UILabel = UILabel()
-            label.setTranslatesAutoresizingMaskIntoConstraints(false)
-            label.text = "Vibrancy"
-            label.font = UIFont(name: "HelveticaNeue-Bold", size: 30)
-            label.textAlignment = .Center
-            label.textColor = UIColor.whiteColor()
-            vibrancyView.contentView.addSubview(label)
+            // Add label to the vibrancy view
+            vibrancyEffectView.contentView.addSubview(vibrantLabel)
+            
+            // Add the vibrancy view to the blur view
+            blurEffectView.contentView.addSubview(vibrancyEffectView)
+  
+            //view.addConstraint(NSLayoutConstraint(item: blurEffectView, attribute: NSLayoutAttribute.Height, relatedBy: NSLayoutRelation.Equal, toItem: nil, attribute: NSLayoutAttribute.NotAnAttribute, multiplier: 1, constant: 50))
+            
         }
         
         key.text = peg.valueForKey("key") as? String
